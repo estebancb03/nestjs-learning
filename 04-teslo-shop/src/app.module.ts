918ccb802eceb2364cf358efa88b405d364728fd
estebancb03 <estebanr.castaneda03@gmail.com>
2025-12-27 21:22:06 -0600

    section11

--- 04-teslo-shop/src/app.module.ts
+++ 04-teslo-shop/src/app.module.ts
@@ -3,22 +3,28 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsModule } from './products/products.module';
 import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
+      port: +process.env.DB_PORT,
       database: process.env.DB_NAME,
       username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
+      password: process.env.DB_PASSWORD,      
       autoLoadEntities: true,
-      synchronize: true //false for PROD env
+      synchronize: true,
     }),
+
     ProductsModule,
+
     CommonModule,
+
+    SeedModule,
   ],
 })
 export class AppModule {}
--- 04-teslo-shop/src/products/products.controller.ts
+++ 04-teslo-shop/src/products/products.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe, Query
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-import { PaginationDTO } from 'src/common/dto/pagination.dto';
+import { PaginationDto } from './../common/dtos/pagination.dto';
 
 @Controller('products')
 export class ProductsController {
@@ -14,25 +14,26 @@ export class ProductsController {
   }
 
   @Get()
-  findAll(@Query() paginationDto:PaginationDTO) {
-    return this.productsService.findAll(paginationDto);
+  findAll( @Query() paginationDto:PaginationDto ) {
+    // console.log(paginationDto)
+    return this.productsService.findAll( paginationDto );
   }
 
-  @Get(':id')
-  findOne(@Param('id') term: string) {
-    return this.productsService.findOne(term);
+  @Get(':term')
+  findOne(@Param( 'term' ) term: string) {
+    return this.productsService.findOnePlain( term );
   }
 
   @Patch(':id')
   update(
-    @Param('id', ParseUUIDPipe) id: string, 
+    @Param('id', ParseUUIDPipe ) id: string, 
     @Body() updateProductDto: UpdateProductDto
   ) {
-    return this.productsService.update(id, updateProductDto);
+    return this.productsService.update( id, updateProductDto );
   }
 
   @Delete(':id')
-  remove(@Param('id', ParseUUIDPipe) id: string) {
-    return this.productsService.remove(id);
+  remove(@Param('id', ParseUUIDPipe ) id: string) {
+    return this.productsService.remove( id );
   }
 }
